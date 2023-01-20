afc95ac0f80eadc7541941e8443500b64dc1d75e
FelipeOropeza <felipe.oropeza@etec.sp.gov.br>
2023-01-20 20:02:25 -0300

    Busca

--- erro2.php
+++ erro2.php
@@ -25,9 +25,7 @@
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-sm-4 col-sm-offset-4 text-center">
-				<h2>Email já em uso na Loja!!!</h2>
-				<a href="formusuario.php" class="btn btn-block btn-info" role="button">Tentar Novamente</a>
-				<a href="esquecisenha.php" class="btn btn-block btn-primary" role="button">Esqueci a senha</a>			
+				<h2>Nenhum livro foi  encontrado !!!</h2>		
 			</div>
 		</div>
 	</div>
